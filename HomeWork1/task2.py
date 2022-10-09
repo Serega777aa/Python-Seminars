e82f316108c9196f416aa4fda858e2828210a7ae
Your Name <serega777aa@mail.ru>
2022-10-09 13:25:01 +0300

    added changes task4

--- HomeWork1/task2.py
+++ HomeWork1/task2.py
@@ -1,5 +1,5 @@
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            result = not(x or y or z) == (not bool(x) and not y and not z)   
+            result = not(x or y or z) == (not x and not y and not z)   
             print(f'x = {(x)}   y = {(y)}   z = {z}  result = {int(result)}')
